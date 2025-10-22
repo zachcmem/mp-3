578f929a1bc7cf5209a0380d7568a15177d4c112
zachcmem <zachcm@bu.edu>
2025-10-21 23:27:37 -0400

    Update ExpCaption1.tsx

--- src/divs/ExpCaption1.tsx
+++ src/divs/ExpCaption1.tsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import vandyk2 from "../images/vandyk2.JPG";
 
 
 const CaptionWrapper = styled.div`
@@ -29,33 +28,11 @@ const CaptionP = styled.p`
     }
 `;
 
-const CaptionImage = styled.img`
-
-    border-width: 10px;
-    border-style: solid;
-    border-color: #FFF0C4;
-    display: table-cell;
-    vertical-align: top;
-    width: 35vw;   
-    height: auto + 50px; 
-    max-width: 300px;  
-    min-width: 150px;  
-    margin-left: 40px;
-    margin-right: 40px;
-
-    @media screen and (max-width: 750px){
-        display: block;
-        margin: 0 auto 15px auto;
-        width: 50vw;  
-        height: auto;
-    }
-`;
 
 export default function ExpCaption1(){
 
     return(
         <CaptionWrapper>
-            <CaptionImage src={vandyk2} alt=""/>
             <CaptionP>Whether you’re designing a new system or looking for retrofit options to improve your operation, Van Dyk is committed to finding a solution that fits 
                         your needs and goals. As an After Sales Equipment Engineer Data Engineer, I applied software engineering with data analytics skills in order to provide 
                         industry standard data representation for project managers and services techs. Working alongside industry professionals I was able to learn relevant technical 
